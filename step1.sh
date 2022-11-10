b8c4b30271d34c5274bcb7f53a4e09eb5021ac73
HoangMH <maihuyhoang13042002@gmail.com>
2022-11-10 22:56:52 +0700

    delete junk files

--- step1.sh
+++
@@ -1,6 +0,0 @@
-git config svn-remote.svn.fetch trunk:refs/remotes/svn/trunk
-git config svn-remote.svn.branches branches/*:refs/remotes/svn/*
-git config svn-remote.svn.tags tags/*:refs/remotes/svn/tags/*
-
-rm .git/refs/remotes/*
-git svn fetch
--- step3.sh
+++
@@ -1,3 +0,0 @@
-git config receive.denyCurrentBranch ignore
-git config receive.denyNonFastForwards true
-git config push.default current
